e90caaabc76945fe4d307f7bb1dd1eeb0afdae2a
Ernest Irabor <irabeny89@gmail.com>
2022-04-26 13:10:39 +0100

    update config

--- config.ts
+++ config.ts
@@ -1,5 +1,6 @@
 const config = {
-  apiKey: process.env.OMDB_API_KEY!
-}
+  apiKey: process.env.OMDB_API_KEY || process.env.OMDB_API_KEY,
+  network: process.env.NETWORK || process.env.NEXT_PUBLIC_NETWORK,
+};
 
-export default config
+export default config;
