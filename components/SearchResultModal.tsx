4efd13bb79589b441cbac733f34913e975bfa611
Ernest Irabor <irabeny89@gmail.com>
2022-04-26 13:12:13 +0100

    add search result modal component

---
+++ components/SearchResultModal.tsx
@@ -0,0 +1,29 @@
+import { useReactiveVar } from "@apollo/client";
+import { filmSearchResultVar } from "graphql/reactiveVars";
+import Modal from "react-bootstrap/Modal";
+import { CgSearchFound } from "react-icons/cg";
+import styles from "styles/Home.module.css";
+import { SearchResultModalPropsType } from "types";
+import MovieCard from "./MovieCard";
+
+export default function SearchResultModal({
+  show,
+  setShow,
+}: SearchResultModalPropsType) {
+  const { Search: searchResults } = useReactiveVar(filmSearchResultVar);
+
+  return (
+    <Modal show={show} onHide={() => setShow(false)} size="lg">
+      <Modal.Header closeButton>
+        <Modal.Title>
+          <CgSearchFound /> Search results...
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className={styles.grid}>
+        {searchResults.map((movie) => (
+          <MovieCard key={movie.imdbID} {...movie} />
+        ))}
+      </Modal.Body>
+    </Modal>
+  );
+}
