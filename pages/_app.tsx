ee041d542c0875a944734b78e83f64bf25d28691
Ernest Irabor <irabeny89@gmail.com>
2022-04-26 06:26:07 +0100

    update root app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ApolloProvider } from "@apollo/client";
+import { SSRProvider } from "react-bootstrap";
+import client from "graphql/client";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider {...{ client }}>
+      <SSRProvider>
+        <Component {...pageProps} />;
+      </SSRProvider>
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
