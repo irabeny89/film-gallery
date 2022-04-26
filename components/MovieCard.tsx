fe0f4e8d4a6e640c2b689892c39da7700e497781
Ernest Irabor <irabeny89@gmail.com>
2022-04-26 13:11:47 +0100

    add movie card

---
+++ components/MovieCard.tsx
@@ -0,0 +1,29 @@
+import styles from "styles/Home.module.css";
+import { MovieType } from "types";
+import Image from "next/image";
+import { HiArchive, HiCalendar, HiFilm, HiHashtag } from "react-icons/hi";
+import config from "config";
+
+export default function MovieCard({
+  Title,
+  Poster,
+  Type,
+  Year,
+  imdbID,
+}: MovieType) {
+  const imageSrc = (config.network === "offline") ? "/vercel.svg" : Poster;
+  
+  return (
+    <div className={styles.card}>
+      <div>
+        <HiHashtag /> {imdbID} | <HiArchive /> {Type} | <HiCalendar /> {Year}
+      </div>
+      <h4 className="text-center my-2">
+        <HiFilm /> {Title}
+      </h4>
+      <div>
+        <Image src={imageSrc} alt="movie poster" width={200} height={250} />
+      </div>
+    </div>
+  );
+}
